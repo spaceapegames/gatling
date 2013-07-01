ccbdf169e4bafe09c2cd338955b36eba54ed5461
keren <ke.lane.ren@gmail.com>
2013-07-01 15:11:02 +0100

    updated match making tests

---
+++ gatling-panda/src/test/resources/data/basic/DynamoMatchSimulation.scala
@@ -0,0 +1,35 @@
+package data.basic
+
+import io.gatling.http.Predef._
+import io.gatling.core.Predef._
+import com.spaceape.panda.proto.Commands.{ReqRepType, BaseReq, ReqPlayersToAttack}
+
+class DynamoMatchSimulation {
+  val httpProtocol = http
+    .baseURL("http://loadtest-panda-game-service-6.use1a.apelabs.net:8024")
+    .shareConnections
+
+  val scn = scenario("DynamoMatchSimulation")
+    .exec(
+      http("making")
+      .post("/v1/reqrep")
+      .byteArrayBody(session => getByteArrayBody(session))
+  )
+
+    	setUp(scn.inject(constantRate(2 usersPerSec) during (1 seconds))).protocols(httpProtocol)
+  //  setUp(scn.inject(rampRate(1000 usersPerSec) to (4000 usersPerSec) during (10 seconds), constantRate(4000 usersPerSec) during (15 seconds))).protocols(httpProtocol)
+
+  val getByteArrayBody = (session: Session) => {
+    val ticket = getTicketFromSession(session)
+    val req = ReqPlayersToAttack.newBuilder().build()
+    val baseReq = BaseReq.newBuilder().setId(0).setReqPlayersToAttack(req).setType(ReqRepType.PlayersToAttack).setAuthenticationTicket(ticket).build()
+    baseReq.toByteArray
+  }
+
+  def getTicketFromSession(session: Session): String = {
+    session("ticket").asOption[String] match {
+      case Some(ticket) => ticket
+      case None => getTicketForClide(session("clide").as[String], session)
+    }
+  }
+}
